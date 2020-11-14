67c49d9b30f3551d70a2ac7ba49722caf445c380
Martinique <me@martidolce.com>
2020-11-14 15:00:29 -0500

    commit tree 10a31569f7670c261bbd3cd2adb2c6b0e54ef0de

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,53 @@
+# frozen_string_literal: true
 
+# Author: Martinique Dolce
+# Course: Flatiron School 2020, November 9 - 20201, April 2021
+# Contact: me@martidolce.com | https://modis.martidolce.com
+
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort! { |x, y| y <=> x }
+end
+
+def sort_array_char_count(array)
+  sort_array_asc(array).sort do |left, right|
+    left.length <=> right.length
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse!
+end
+
+def kesha_maker(array)
+  array.each do |swap|
+    swap[2] = '$'
+  end
+end
+
+def find_a(array)
+  array.select { |word| word.start_with?('a') }
+end
+
+def sum_array(array)
+  array.inject(0) { |sum, x| sum + x }
+end
+
+def add_s(array)
+  str = 's'
+  array.collect do |word|
+    if array[1] == word
+      word
+    else
+      word + str
+    end
+  end
+end
